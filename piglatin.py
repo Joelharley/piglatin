0dfd665775943313ffab7af892570d5d2acb003b
Joel Harley <jharley@opengamingsolutions.com>
2016-12-20 14:47:07 -0800

    chore(debug/py)put the whole thing together and fixed bug with def translator(word)

--- piglatin.py
+++ piglatin.py
@@ -1,10 +1,13 @@
 import string
-sentance = 'try a salad for lunch, and dinner it is a nice treat ' #what we're translating
-cut = 0					# integer to cut at the ' ' positions
-lastcut = 0				# integer to cut at the last ' ' position			
-firstLetterSlice = ''	# the first letter is cut by this
-finalProduct = ''		# final product
-word = ''
+sentance = '''
+This is my English to Pig Latin translator!
+ It will do the proper translations for words beginning with vowels, 
+ and will account for most punctuation and re-capitalize words at the start of sentances.
+ It's pretty neat!
+'''
+sentance = sentance.split() #turns sentance into a list for easier translation
+roughpig = ''				#will be the rough translation before punctuation sorting
+pig = ''					#final translation
 vowels = [
 'a','A',
 'e','E',
@@ -12,8 +15,8 @@ vowels = [
 'o','O',
 'u','U']
 punctuation = [
-'!',',','.','?','/',
-'"',"'",';',':']
+'!',',','.','?',
+'"',';',':','/']
 consonantCaps = [
 'B','C','D','F',
 'G','H','J','K',
@@ -21,42 +24,35 @@ consonantCaps = [
 'Q','R','S','T',
 'U','V','W','X',
 'Y','Z']
-
-def sentanceCutter(sentance):
-	'slices input sentance into individual words'
-	for i in sentance:
-		global cut
-		global lastcut
-		cut += 1
-		if i == ' ':
-			word = sentance[lastcut:cut]
-	return word
-
 def translator(word):
 	global vowels
 	global consonantCaps
 	if word[0] in vowels:
-		wordProduct = word + 'yay'
+		translatorProduct = word + 'yay'
 	elif word[0] in consonantCaps:
-		firstletter = lower(word[0])
-		word = word[:0]
-		wordProduct = word + firstletter + 'ay'
-		wordProduct = capitalize(wordProduct)
+		firstletter = string.lower(word[0])
+		word = word[1:]
+		translatorProduct = word + firstletter + 'ay'
+		translatorProduct = string.capitalize(translatorProduct)
 	else:
 		firstletter = word[0]
-		word = word[:0]
-		wordProduct = word + firstletter = 'ay'
-	return wordProduct
-
+		word = word[1:]
+		translatorProduct = word + firstletter + 'ay'
+	return translatorProduct
 def punctuationcheck(word):
 	position = 0
 	for i in word:
 		position = position + 1
 		if i in punctuation:
 			x = word[position-1:position]
-			word = word[position-1:]+word[:position]+x
-	return word  
+			word = word[:position-1]+word[position:]+x		
+	return word 
+for i in sentance:
+	roughpig = roughpig + translator(i) + ' '
+roughpig = roughpig.split()
+for i in roughpig:
+	pig = pig + punctuationcheck(i) + ' '
+print(pig)
 
 
 
-sentanceCutter(sentance)
