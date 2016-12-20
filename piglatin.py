e8eaffed565c5be6d9c085f143036c178c85468b
Joel Harley <jharley@opengamingsolutions.com>
2016-12-20 09:30:30 -0800

    chore(py)basic idea of code, need to optimize

--- piglatin.py
+++ piglatin.py
@@ -1,17 +1,39 @@
-sentance = 'try '
-cut = 0		# integer to cut at the ' ' positions
-lastcut = 0	# integer to cut at the last ' ' position
-translator = ' '
-translation = ' '
+sentance = 'try a salad for lunch, and dinner it is a nice treat '
+cut = 0					# integer to cut at the ' ' positions
+lastcut = 0				# integer to cut at the last ' ' position
+translator = ' '		# the first letter is cut by this
+translation = ' '		# end product of individual word
+fullTranslate = ''		# final product
+
+
 for i in sentance:
 	cut += 1
 	if i == ' ':
-		word = sentance[lastcut:(cut-1)]
+		word = sentance[lastcut:(cut)]
 		lastcut = cut
-			if word[0] == 'a' or 'e' or 'i' or 'o' or 'u' or 'A' or 'E' or 'I' or 'O' or 'U':
-				translation = word + yay
+		if word[len(word)-1] == ','\
+							 or '.'\
+							 or '?'\
+							 or '!'\
+							 or '"'\
+							 or "'"\
+							 or '/':	# catches punctuation silly business
+			punctuation = word[-1]
+			word = word[:-1]
+			if word[0] == 'a'\
+						or 'e'\
+						or 'i'\
+						or 'o'\
+						or 'u':	#	-	- vowel proper pronounciation for punctuation
+				translation = word + 'yay'
 			else:
-				translator = word[0:1]
-				translation = (word[1:] + translator + 'ay')
-			print(translation)
+				translation = word[1:] + translator + 'ay ' + punctuation
+		if word[0] == 'a' or 'e' or 'i' or 'o' or 'u':	#	-	-	-	-	-	-	-	- vowel proper pronounciation
+			translation = word + 'yay'
+		else:	#	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	-	- all other cases
+			translator = word[0:1]
+			translation = (word[1:] + translator + 'ay ')
+			fullTranslate = fullTranslate + translation + ' '
+print(len(fullTranslate))
+print(fullTranslate)
 		
